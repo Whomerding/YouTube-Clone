8642ee86cdaadca427db16fe022e60d8461d8fd1
Whomerding <wendy.homerding@gmail.com>
2023-04-14 13:06:22 -0400

    added another API call in order to get aall replies + post replies+ reply mapper

--- frontend/src/components/CommentPresenter/CommentPresenter.jsx
+++ frontend/src/components/CommentPresenter/CommentPresenter.jsx
@@ -1,36 +1,46 @@
 import React, { useState, useEffect } from "react";
-import './CommentPresenter.css'
-// import ReplyMapper from "../ReplyMapper/ReplyMapper";
-// import axios from "axios";
-const CommentPresenter = ({ comment}) => {
-// const [replies, setReplies] = useState([])
+import axios from "axios";
+import ReplyMapper from "../ReplyMapper/ReplyMapper";
+import PostReply from "../PostReply/PostReply";
 
-  
+const CommentPresenter = ({ comment }) => {
+  const [replies, setReplies] = useState([]);
 
+  useEffect(() => {
+    getAllReplies();
+  }, []);
 
-// useEffect(()=>{
-// //     getAllReplies();
-// // }, []);
+  async function getAllReplies() {
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:8000/api/reply/${comment.id}/`
+      );
+      setReplies(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-// // async function getAllReplies(){
-// //     try {
-// //         const response = await axios.get(`http://127.0.0.1:8000/api/reply/5/`);
-// //         setReplies(response.data);
-      
-        
-// //     } catch (error) {
-// //         console.log (error)
-// //     }
-// //       }  
-  
   return (
     <li>
-      <div style={{textAlign: 'left'}}>
-        <p style={{fontWeight: 'bold', fontStyle: 'italic', fontSize: ',medium'}}>{comment.user.first_name} {comment.user.last_name}</p>
-        <p style={{fontSize: 'small'}}>{comment.text}</p>
-        {/* <ul>
-          <ReplyMapper replies = {replies} />
-        </ul> */}
+      <div style={{ textAlign: "left" }}>
+        <p
+          style={{
+            fontWeight: "bold",
+            fontStyle: "italic",
+            fontSize: ",medium",
+          }}
+        >
+          {comment.user.first_name} {comment.user.last_name}
+        </p>
+        <p style={{ fontSize: "small" }}>{comment.text}</p>
+        <ul>
+        <h3 style= {{fontWeight: "bold", fontSize: "medium"}}>Replies</h3>
+        <ReplyMapper replies = {replies} />
+        <PostReply
+            getAllReplies={getAllReplies}
+            commentId={comment.id}/>
+        </ul>
       </div>
     </li>
   );
