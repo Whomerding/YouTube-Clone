bf61cadf55e6672473cfb9dc331ef21b44f3684c
Whomerding <wendy.homerding@gmail.com>
2023-04-14 12:50:56 -0400

    fixed the video player so that the details and title showed up

--- frontend/src/components/ReplyPresenter/ReplyPresenter.jsx
+++ frontend/src/components/ReplyPresenter/ReplyPresenter.jsx
@@ -1,14 +1,14 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// const ReplyPresenter = ({reply}) => {
-//   return (
-//     <li>
-//       <div style={{textAlign: 'left'}}>
-//         <p style={{fontWeight: 'bold', fontStyle: 'italic', fontSize: ',medium'}}>{reply.user.first_name} {reply.user.last_name}</p>
-//         <p style={{fontSize: 'small'}}>{reply.text}</p>
-//       </div>
-//     </li>
-//   );
-// };
+const ReplyPresenter = ({reply}) => {
+  return (
+    <li style= {{listStyle: 'none'}}>
+      <div style={{textAlign: 'left'}}>
+        <p style={{fontWeight: 'bold', fontStyle: 'italic', fontSize: 'small'}}>{reply.user.first_name} {reply.user.last_name}</p>
+        <p style={{fontSize: 'small'}}>{reply.text}</p>
+      </div>
+    </li>
+  );
+};
 
-// export default ReplyPresenter;
+export default ReplyPresenter;
--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 const VideoPlayer = ({realVideoId}) => {
     return (
         <div>
